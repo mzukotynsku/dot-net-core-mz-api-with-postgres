7213b3be11277a1974333235af920d97e2dd585b
matik <matik104@gmail.com>
2020-06-12 12:41:18 +0200

    add changes to heroku configuration

--- DotNetCoreMZ.API/Installers/DbInstaller.cs
+++ DotNetCoreMZ.API/Installers/DbInstaller.cs
@@ -16,8 +16,35 @@ namespace DotNetCoreMZ.API.Installers
         public void InstallServices(IConfiguration configuration, IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options =>
-                options.UseNpgsql(
-                    configuration.GetConnectionString("DefaultConnection")));
+            {
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                string connStr;
+
+                if (env == "Development")
+                {
+
+                    connStr = configuration.GetConnectionString("DefaultConnection");
+                }
+                else
+                {
+
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                }
+
+                options.UseNpgsql(connStr);
+            }
+                );
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<DataContext>();
